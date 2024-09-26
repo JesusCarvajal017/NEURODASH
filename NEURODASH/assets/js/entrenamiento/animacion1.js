ccafabc596d9564ce516da1ff94e444ff65b7e4e
nataliosorio <nataliaosorio973@gmail.com>
2024-09-26 08:47:21 -0500

    transiciones-nati

---
+++ NEURODASH/assets/js/entrenamiento/animacion1.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    setTimeout(() => {
+      document.querySelector('.estilo').classList.add('animate');
+    }, 100); // Puedes ajustar el tiempo de espera si lo deseas
+  });
---
+++ NEURODASH/assets/js/entrenamiento/transition1.js
@@ -0,0 +1,5 @@
+document.body.classList.add('exit-animation2');
+
+setTimeout(() => {
+    document.body.classList.remove('exit-animation2');
+}, 1500);  

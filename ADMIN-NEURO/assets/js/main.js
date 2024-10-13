cfd04b10324590726e2e11218ee64ddcfe177c4a
JesusCarvajal <jesusfernandocarvajal2018@gmail.com>
2024-10-13 08:28:08 -0500

    model-client

---
+++ ADMIN-NEURO/assets/js/main.js
@@ -0,0 +1,11 @@
+window.addEventListener('load', ()=>{
+    
+})
+
+
+
+
+
+let barra = document.querySelector('.sidebar-wrapper');
+
+barra.innerHTML = barraLateral;
--- NEURODASH/backup/respaldo.sql
+++ NEURODASH/backup/respaldo.sql
@@ -1,13 +1,14 @@
 CREATE TABLE usuario (
 	user_Id BIGINT NOT NULL UNIQUE,
-	user_name VARCHAR NOT NULL,
+	user_name VARCHAR() NOT NULL UNIQUE,
 	user_email VARCHAR NOT NULL UNIQUE,
 	user_avatar VARCHAR NOT NULL DEFAULT 'img_system.png',
-	user_exp BIGINT NOT NULL,
-	rgo_id INTEGER NOT NULL,
+	user_exp BIGINT NULL,
+	rgo_id INTEGER NULL,
 	tp_user_id INTEGER NOT NULL,
 	PRIMARY KEY(user_Id)
 );
+
 
 CREATE TABLE login (
 	loginId INTEGER NOT NULL UNIQUE,
@@ -16,13 +17,15 @@ CREATE TABLE login (
 	PRIMARY KEY(loginId)
 );
 
+
 CREATE TABLE configUsuario (
 	config_Id INTEGER NOT NULL UNIQUE,
 	user_id INTEGER NOT NULL,
-	config_sonido BOOLEAN NOT NULL,
-	config_musica CHAR NOT NULL,
+	config_sonido CHAR(3) NOT NULL,
+	config_musica CHAR(3) NOT NULL,
 	PRIMARY KEY(config_Id)
 );
+
 
 CREATE TABLE rangos (
 	rgo_Id INTEGER NOT NULL UNIQUE,
@@ -33,11 +36,13 @@ CREATE TABLE rangos (
 	PRIMARY KEY(rgo_Id)
 );
 
+
 CREATE TABLE niveles (
 	nvel_Id INTEGER NOT NULL UNIQUE,
 	nvel_nombre VARCHAR NOT NULL,
 	PRIMARY KEY(nvel_Id)
 );
+
 
 CREATE TABLE modoJuego (
 	mdo_juegoId INTEGER NOT NULL UNIQUE,
@@ -45,11 +50,13 @@ CREATE TABLE modoJuego (
 	PRIMARY KEY(mdo_juegoId)
 );
 
+
 CREATE TABLE tipoUsuario (
 	tp_user_Id INTEGER NOT NULL UNIQUE,
 	tp_user_name VARCHAR NOT NULL,
 	PRIMARY KEY(tp_user_Id)
 );
+
 
 CREATE TABLE salasPrivadas (
 	sla_privadaId INTEGER NOT NULL UNIQUE,
@@ -59,6 +66,7 @@ CREATE TABLE salasPrivadas (
 	PRIMARY KEY(sla_privadaId)
 );
 
+
 CREATE TABLE configSala (
 	cfg_salaId INTEGER NOT NULL UNIQUE,
 	cfg_cantidadJugadores SMALLINT NOT NULL,
@@ -67,6 +75,7 @@ CREATE TABLE configSala (
 	dfi_id INTEGER NOT NULL,
 	PRIMARY KEY(cfg_salaId)
 );
+
 
 CREATE TABLE dificultadEntrenamiento (
 	dfi_id INTEGER NOT NULL UNIQUE,
@@ -79,12 +88,22 @@ CREATE TABLE dificultadEntrenamiento (
 	PRIMARY KEY(dfi_id)
 );
 
+
 CREATE TABLE salaJugadores (
 	sla_jug_id INTEGER NOT NULL UNIQUE,
 	sla_privadaId INTEGER,
 	user_id INTEGER,
 	PRIMARY KEY(sla_jug_id)
 );
+
+
+CREATE TABLE notificaciones (
+	nf_id INTEGER NOT NULL UNIQUE,
+	user_id INTEGER NOT NULL,
+	nf_descripcion VARCHAR NOT NULL,
+	PRIMARY KEY(nf_id)
+);
+
 
 ALTER TABLE dificultadEntrenamiento
 ADD FOREIGN KEY(mdo_juegoId) REFERENCES modoJuego(mdo_juegoId)
@@ -128,4 +147,12 @@ ON UPDATE NO ACTION ON DELETE NO ACTION;
 
 ALTER TABLE usuario
 ADD FOREIGN KEY(tp_user_id) REFERENCES tipoUsuario(tp_user_Id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE salaJugadores
+ADD FOREIGN KEY(user_id) REFERENCES usuario(user_Id)
+ON UPDATE NO ACTION ON DELETE NO ACTION;
+
+ALTER TABLE notificaciones
+ADD FOREIGN KEY(user_id) REFERENCES usuario(user_Id)
 ON UPDATE NO ACTION ON DELETE NO ACTION;
